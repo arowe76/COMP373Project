e7b6b97213971d8ae70bb0a076f43ddab2d526a3
Andrew Rowe <arowe@luc.edu>
2025-11-29 13:07:20 -0600

    Added CachedWeather

---
+++ app/src/main/java/com/frsarker/weatherapp/data/CachedWeather.java
@@ -0,0 +1,12 @@
+package com.frsarker.weatherapp.data;
+
+public class CachedWeather {
+    private String cityName;
+    private String description;
+    private String temperature;
+    private String lastUpdateMillis;
+
+
+    // Constructors, Getters, Setters...
+
+}
