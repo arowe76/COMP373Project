460796f4f572442deb77e14f40bbc3b4482e81b1
Andrew Rowe <arowe@luc.edu>
2025-12-01 13:27:17 -0600

    Updated WeatherSyncWorker

--- app/src/main/java/com/frsarker/weatherapp/workers/WeatherSyncWorker.java
+++ app/src/main/java/com/frsarker/weatherapp/workers/WeatherSyncWorker.java
@@ -2,13 +2,15 @@ package com.frsarker.weatherapp.workers;
 
 import android.content.Context;
 
+
 /**
  * This is a temporary stub for WeatherSyncWorker so the project compiles.
+ * TODO: It will be replaced later for auto-sync (network outage).
  */
 public class WeatherSyncWorker {
-
     public static void enqueueForCity(Context context, String cityName) {
         // TODO: implement WorkManager job here later...
         // For now this is just a placeholder so code compiles.
+
     }
 }
